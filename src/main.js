33a41a79b6241403d9b09bc8475cd8d7c1bceaa3
Ichi0021 <ichii0518@gmail.com>
2021-08-28 14:06:59 +0900

    firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlA3tJm0AhThqNAPmTaKsXx-2BXX-dZHM",
+  authDomain: "vue-bbs-ed718.firebaseapp.com",
+  projectId: "vue-bbs-ed718",
+  storageBucket: "vue-bbs-ed718.appspot.com",
+  messagingSenderId: "801096101771",
+  appId: "1:801096101771:web:46c9cf21670e4435aac98e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 createApp(App).mount('#app')
